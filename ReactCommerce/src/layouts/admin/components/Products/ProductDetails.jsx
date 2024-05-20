112068f41e91bfa9d75573be5bf7df78fe987bbf
assadyoussef0 <amine150assad@gmail.com>
2024-05-20 20:30:21 +0100

    fix some pb on design

--- ReactCommerce/src/layouts/admin/components/Products/ProductDetails.jsx
+++ ReactCommerce/src/layouts/admin/components/Products/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Breadcrumbs } from '@mui/material';
 import Slider from 'react-slick';
-
+import StoreIcon from '@mui/icons-material/Store';
 function ProductDetails() {
     const productSliderOptions = {
         dots: false,
@@ -28,15 +28,15 @@ function ProductDetails() {
                     Dashboard Home
                 </div>
             </div>
-            <div className="cardP ">
+            <div className="cardP productDetailsSection">
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-5">
                         <div className="sliderWrapper pt-3 pb-4 pl-4 pr-4">
-                            <h5 className='mb-3' > Product Gallery  </h5>
-                            <Slider {...productSliderOptions} className='sliderBig mn-2 ' >
+                            <h5 className='mb-4' > Product Gallery  </h5>
+                            <Slider {...productSliderOptions} className='sliderBig mn-2' >
                                 <div className="item">
                                     <img
-                                        src="https: //assets.adidas.com/images/w_600,f_auto,q_auto/bd43ce71f589498ab6b1aad6009a0e6e_9366/Superstar_Shoes_White_EG4958_07_standard.jpg"
+                                        src="https://assets.adidas.com/images/w_600,f_auto,q_auto/bd43ce71f589498ab6b1aad6009a0e6e_9366/Superstar_Shoes_White_EG4958_07_standard.jpg"
                                         alt="Product Image"
                                         className="w-100"
                                     />
@@ -63,11 +63,179 @@ function ProductDetails() {
                                         alt="Product Image"
                                         className="w-100"
                                     />
+                                </div>  <div className="item">
+                                    <img
+                                        src="https://assets.adidas.com/images/w_600,f_auto,q_auto/bd43ce71f589498ab6b1aad6009a0e6e_9366/Superstar_Shoes_White_EG4958_07_standard.jpg"
+                                        alt="Product Image"
+                                        className="w-100"
+                                    />
+                                </div>  <div className="item">
+                                    <img
+                                        src="https://assets.adidas.com/images/w_600,f_auto,q_auto/bd43ce71f589498ab6b1aad6009a0e6e_9366/Superstar_Shoes_White_EG4958_07_standard.jpg"
+                                        alt="Product Image"
+                                        className="w-100"
+                                    />
                                 </div>
                             </Slider>
                         </div>
                     </div>
-                    <div className="col-md-8">
+                    <div className="col-md-7">
+                        <div className="pt-3 pb-3 pl-4 pr-4">
+                            <h5 className="mb-4">Product Details</h5>
+                            <h4 style={{ lineHeight: '32px' }} >
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, cupiditate quas
+                            </h4>
+                            <div className="productInfo mt-3">
+                                <div className="row mb-2">
+                                    <div className="col-sm-5 d-flex align-items-center">
+                                        <span className="icon">
+                                            <StoreIcon />
+                                        </span>
+                                        <span className="name">
+                                            Brand
+                                        </span>
+                                    </div>
+                                    <div className="col-sm-7">
+                                        :  <span>Adidas </span>
+                                    </div>
+                                </div>
+                                <div className="row mb-2">
+                                    <div className="col-sm-5 d-flex align-items-center">
+                                        <span className="icon">
+                                            <StoreIcon />
+                                        </span>
+                                        <span className="name">
+                                            Category
+                                        </span>
+                                    </div>
+                                    <div className="col-sm-7 ">
+                                        :  <span>Men </span>
+                                    </div>
+                                </div>
+                                <div className="row mb-2">
+                                    <div className="col-sm-5 d-flex align-items-center">
+                                        <span className="icon">
+                                            <StoreIcon />
+                                        </span>
+                                        <span className="name">
+                                            Tags
+                                        </span>
+                                    </div>
+                                    <div className="col-sm-7 ">
+                                        :  <span>Adidas </span>
+                                    </div>
+
+                                </div>
+                                <div className="row mb-2">
+                                    <div className="col-sm-5 d-flex align-items-center">
+                                        <span className="icon">
+                                            <StoreIcon />
+                                        </span>
+                                        <span className="name">
+                                            Color
+                                        </span>
+                                    </div>
+                                    <div className="col-sm-7 ">
+                                        :  <span>
+                                            <ul className="list list-inline colors sml">
+                                                <li className="list-inline-item">
+                                                    <span>
+                                                        Black
+                                                    </span>
+                                                </li>
+                                                <li className="list-inline-item">
+                                                    <span>
+                                                        white
+                                                    </span>
+                                                </li>
+                                                <li className="list-inline-item">
+                                                    <span>
+                                                        Gray
+                                                    </span>
+                                                </li><li className="list-inline-item">
+                                                    <span>
+                                                        Gray
+                                                    </span>
+                                                </li><li className="list-inline-item">
+                                                    <span>
+                                                        Gray
+                                                    </span>
+                                                </li><li className="list-inline-item">
+                                                    <span>
+                                                        Gray
+                                                    </span>
+                                                </li>
+                                            </ul> </span>
+                                    </div>
+                                </div>
+                                <div className="row mb-2">
+                                    <div className="col-sm-5 d-flex align-items-center">
+                                        <span className="icon">
+                                            <StoreIcon />
+                                        </span>
+                                        <span className="name">
+                                            Size
+                                        </span>
+                                    </div>
+                                    <div className="col-sm-7 ">
+                                        :  <span>Adidas </span>
+                                    </div>
+                                </div>
+                                <div className="row mb-2">
+                                    <div className="col-sm-5 d-flex align-items-center">
+                                        <span className="icon">
+                                            <StoreIcon />
+                                        </span>
+                                        <span className="name">
+                                            Price
+                                        </span>
+                                    </div>
+                                    <div className="col-sm-7 ">
+                                        :  <span>Adidas </span>
+                                    </div>
+                                </div>
+                                <div className="row mb-2">
+                                    <div className="col-sm-5 d-flex align-items-center">
+                                        <span className="icon">
+                                            <StoreIcon />
+                                        </span>
+                                        <span className="name">
+                                            Stock
+                                        </span>
+                                    </div>
+                                    <div className="col-sm-7 ">
+                                        :  <span>Adidas </span>
+                                    </div>
+                                </div>
+                                <div className="row mb-2">
+                                    <div className="col-sm-5 d-flex align-items-center">
+                                        <span className="icon">
+                                            <StoreIcon />
+                                        </span>
+                                        <span className="name">
+                                            Review
+                                        </span>
+                                    </div>
+                                    <div className="col-sm-7 ">
+                                        :  <span>Adidas </span>
+                                    </div>
+                                </div>
+                                <div className="row mb-2">
+                                    <div className="col-sm-5 d-flex align-items-center">
+                                        <span className="icon">
+                                            <StoreIcon />
+                                        </span>
+                                        <span className="name">
+                                            Published
+                                        </span>
+                                    </div>
+                                    <div className="col-sm-7 ">
+                                        :  <span>Adidas </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
