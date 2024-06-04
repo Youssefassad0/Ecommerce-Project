00ff6e48e935cd28d8c8dc484e4cc1f71f7423d4
assadyoussef0 <amine150assad@gmail.com>
2024-06-05 00:08:04 +0100

    almost complet user list

--- ReactCommerce/src/layouts/admin/components/Users/UsersPage.jsx
+++ ReactCommerce/src/layouts/admin/components/Users/UsersPage.jsx
@@ -1,22 +1,74 @@
-import React, { useEffect, useState } from 'react'
-import { Box, useTheme } from '@mui/material';
-import {DataGrid} from "@mui/x-data-grid"
+import React, { useEffect, useState } from 'react';
+import { Box } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import axios from 'axios';
-function UsersPage() {
-  const [data,setData]=useState([]);
-  const [loading,setLoading]=useState(true);
-  useEffect(()=>{
-async function getData(){
-  await axios.get('http://localhost:8001/api/users').then(res=>setData(res.data)).catch(err=>{
-    console.log('UN ERROR DE : '+err);
-  })
-  setLoading(false)
 
-}  getData();
-  },[])
+function UsersPage() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('http://localhost:8001/api/users');
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const columns = [
+    { field: 'id', headerName: 'ID', flex: 0.5 },
+    { field: 'name', headerName: 'Name', flex: 0.5 },
+    { field: 'email', headerName: 'Email', flex: 1 },
+    { field: 'mobile', headerName: 'Phone Number', flex: 0.5 },
+    {
+      field: 'location',
+      headerName: 'Country',
+      flex: 0.4,
+    },
+  ];
+
+  const formatPhoneNumber = (phoneNumber) => {
+    return phoneNumber.replace(/^(\d{3})(\d{3})(\d{4})$/, '($1) $2-$3');
+  };
+
   return (
-    <div>UsersPage</div>
-  )
+    <div>
+      <Box m="1.5rem 2.5rem">
+        <h1>List Of Customers</h1>
+        <Box mt="40px">
+          <DataGrid
+            rows={data}
+            columns={columns}
+            getRowId={(row) => row.id}
+            loading={loading}
+            autoHeight
+            pageSize={10}
+            rowsPerPageOptions={[10, 25, 50]}
+            disableSelectionOnClick
+            disableColumnMenu
+            density="compact"
+            components={{
+              Toolbar: () => null, // Hide toolbar
+            }}
+            localeText={{
+              noRowsLabel: 'No customers found',
+            }}
+            renderCell={(params) => {
+              if (params.field === 'mobile') {
+                return formatPhoneNumber(params.value);
+              }
+              return params.value;
+            }}
+          />
+        </Box>
+      </Box>
+    </div>
+  );
 }
 
-export default UsersPage
+export default UsersPage;
